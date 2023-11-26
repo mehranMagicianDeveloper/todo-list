e17b43a564ecc0bc0d901885be84af158d9168aa
Mehran Hamzei <mehran@foodapp.ee>
2023-11-26 11:59:49 +0330

    searching algorythm updated

--- src/features/todo/TodosList.js
+++ src/features/todo/TodosList.js
@@ -6,21 +6,18 @@ function TodosList() {
   const todos = useSelector(selectTodos);
   const searchData = useSelector(getSearchData);
 
-  console.log("====================================");
-  console.log(`serching: ${searchData}`);
-  console.log("====================================");
-
   const renderTodos = todos.map((todo) => {
     let finalData = <></>;
     if (searchData === "") {
       finalData = <TodoItem key={todo.id} todo={todo} />;
     } else {
-      if (todo.title.includes(searchData)) {
+      if (todo.title.toLowerCase().includes(searchData)) {
         finalData = <TodoItem key={todo.id} todo={todo} />;
       }
     }
     return finalData;
   });
+
   return <main>{renderTodos}</main>;
 }
 
